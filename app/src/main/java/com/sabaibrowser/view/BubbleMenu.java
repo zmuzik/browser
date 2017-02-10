5dc072ec90a797f72efc4d742bb352e1c73506d6
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-02-10 17:34:01 +0700

    update icon on menu open

--- app/src/main/java/com/sabaibrowser/view/BubbleMenu.java
+++ app/src/main/java/com/sabaibrowser/view/BubbleMenu.java
@@ -5,11 +5,13 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.sabaibrowser.R;
 
 public class BubbleMenu extends FrameLayout {
+
+    boolean isOpen = false;
+    Bubble mainFab;
 
     public BubbleMenu(Context context) {
         super(context);
@@ -23,7 +25,7 @@ public class BubbleMenu extends FrameLayout {
 
     void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.bubble_menu, this);
-        Bubble mainFab = (Bubble) findViewById(R.id.main_fab);
+        mainFab = (Bubble) findViewById(R.id.main_fab);
         mainFab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +35,11 @@ public class BubbleMenu extends FrameLayout {
     }
 
     void toggleOpenMenu() {
-        Toast.makeText(getContext(), "toggle", Toast.LENGTH_SHORT).show();
+        if (isOpen) {
+            mainFab.setImageDrawable(getResources().getDrawable(R.drawable.ic_close_white));
+        } else {
+            mainFab.setImageDrawable(getResources().getDrawable(R.drawable.logo_fab_icon));
+        }
+        isOpen = !isOpen;
     }
 }
