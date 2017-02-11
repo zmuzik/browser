c74c872bda2488c7a6bc1e0c1e05783b624b6972
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-02-11 12:21:06 +0700

    bubble menu phase 2

--- app/src/main/java/com/sabaibrowser/view/BubbleMenu.java
+++ app/src/main/java/com/sabaibrowser/view/BubbleMenu.java
@@ -1,9 +1,11 @@
 package com.sabaibrowser.view;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.sabaibrowser.R;
@@ -23,6 +25,12 @@ public class BubbleMenu extends FrameLayout {
         init();
     }
 
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (isOpen) closeMenu();
+    }
+
     void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.bubble_menu, this);
         mainFab = (Bubble) findViewById(R.id.main_fab);
@@ -32,14 +40,65 @@ public class BubbleMenu extends FrameLayout {
                 toggleOpenMenu();
             }
         });
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isOpen) {
+                    toggleOpenMenu();
+                }
+            }
+        });
     }
 
     void toggleOpenMenu() {
-        if (isOpen) {
-            mainFab.setImageDrawable(getResources().getDrawable(R.drawable.ic_close_white));
+        if (!isOpen) {
+            openMenu();
         } else {
-            mainFab.setImageDrawable(getResources().getDrawable(R.drawable.logo_fab_icon));
+            closeMenu();
         }
-        isOpen = !isOpen;
+    }
+
+    void openMenu() {
+        mainFab.setImageDrawable(getResources().getDrawable(R.drawable.ic_close_white));
+        BubbleScroller scroller = new BubbleScroller(getContext());
+        addView(scroller);
+        invalidate();
+        isOpen = true;
+    }
+
+    void closeMenu() {
+        mainFab.setImageDrawable(getResources().getDrawable(R.drawable.logo_fab_icon));
+        removeViewAt(1);
+        invalidate();
+        isOpen = false;
+    }
+
+    private static class BubbleScroller extends ViewGroup {
+
+        int componentWidthPx;
+        int componentHeightPx;
+
+        public BubbleScroller(Context context) {
+            super(context);
+            setBackgroundColor(getResources().getColor(R.color.bubble_menu_bg));
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            componentWidthPx = ((View) getParent().getParent()).getWidth();
+            componentHeightPx = ((View) getParent().getParent()).getHeight();
+            setMeasuredDimension(componentWidthPx, componentHeightPx);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            final int count = getChildCount();
+            for (int i = 0; i < count; i++) {
+                View child = getChildAt(i);
+                child.layout(0, 0, 0, 0);
+            }
+        }
     }
 }
