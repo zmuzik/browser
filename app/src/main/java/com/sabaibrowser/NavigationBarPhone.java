2dc4c392a6b70c5fd4b60be264734f64dd6b39a3
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-02-14 11:34:39 +0700

    minor fixes of the nav bar

--- app/src/main/java/com/sabaibrowser/NavigationBarPhone.java
+++ app/src/main/java/com/sabaibrowser/NavigationBarPhone.java
@@ -39,7 +39,7 @@ public class NavigationBarPhone extends NavigationBarBase implements
     private ImageView mStopButton;
     private ImageView mMagnify;
     private ImageView mClearButton;
-    private ImageView mFabFilling;
+    //private ImageView mFabFilling;
     private Drawable mStopDrawable;
     private Drawable mRefreshDrawable;
     private String mStopDescription;
@@ -72,7 +72,7 @@ public class NavigationBarPhone extends NavigationBarBase implements
         mClearButton.setOnClickListener(this);
         mMagnify = (ImageView) findViewById(R.id.magnify);
         mTitleContainer = findViewById(R.id.title_bg);
-        mFabFilling = (ImageView) findViewById(R.id.fab_filling);
+        //mFabFilling = (ImageView) findViewById(R.id.fab_filling);
         setFocusState(false);
         Resources res = getContext().getResources();
         mStopDrawable = res.getDrawable(R.drawable.ic_stop);
@@ -205,7 +205,7 @@ public class NavigationBarPhone extends NavigationBarBase implements
             mClearButton.setVisibility(View.GONE);
             mMagnify.setVisibility(View.GONE);
             mTitleContainer.setBackgroundDrawable(null);
-            mFabFilling.setVisibility(View.VISIBLE);
+            //mFabFilling.setVisibility(View.VISIBLE);
             if (mFab != null) {
                 mFab.setVisibility(View.VISIBLE);
             }
@@ -214,14 +214,14 @@ public class NavigationBarPhone extends NavigationBarBase implements
             mStopButton.setVisibility(View.VISIBLE);
             mClearButton.setVisibility(View.GONE);
             mMagnify.setVisibility(View.GONE);
-            mFabFilling.setVisibility(View.GONE);
+            //mFabFilling.setVisibility(View.GONE);
             mFab.setVisibility(View.GONE);
             break;
         case StateListener.STATE_EDITED:
             mStopButton.setVisibility(View.GONE);
             mClearButton.setVisibility(View.VISIBLE);
             mMagnify.setVisibility(View.VISIBLE);
-            mFabFilling.setVisibility(View.GONE);
+            //mFabFilling.setVisibility(View.GONE);
             mFab.setVisibility(View.GONE);
             break;
         }
