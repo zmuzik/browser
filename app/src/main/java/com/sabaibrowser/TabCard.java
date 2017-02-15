ec462a6ba31a3355510c5c5c1ae855c201812d4f
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-02-15 15:09:54 +0700

    turn tabcard into viewgroup

--- app/src/main/java/com/sabaibrowser/TabCard.java
+++ app/src/main/java/com/sabaibrowser/TabCard.java
@@ -6,15 +6,22 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class TabCard extends LinearLayout {
+public class TabCard extends ViewGroup {
 
     TextView mTitle;
     ImageView mThumbnail;
     ImageView mIncognitoIndicator;
+    int mWidth;
+    int mHeight;
+    int mPadding;
+    int mThumbnailWidth;
+    int mThumbnailHeight;
+    int mTitleHeight;
 
     public TabCard(Context context) {
         super(context);
@@ -26,25 +33,67 @@ public class TabCard extends LinearLayout {
         init();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int height = (int) dpToPx(96);
-        int width = (int) dpToPx(160);
-        setMeasuredDimension(width | MeasureSpec.EXACTLY, height | MeasureSpec.EXACTLY);
-    }
-
-    float dpToPx(int dp) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
-    }
-
     private void init() {
-        setOrientation(LinearLayout.VERTICAL);
+        setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
         LayoutInflater.from(getContext()).inflate(R.layout.tab_card, this);
-        setBackgroundColor(getResources().getColor(R.color.light_gray));
+        setBackgroundColor(getResources().getColor(R.color.primary));
+
+        mPadding = (int) getResources().getDimension(R.dimen.nav_tab_padding);
+        mTitleHeight = (int) getResources().getDimension(R.dimen.tab_thumbnail_title_height);
+        mThumbnailWidth = (int) getResources().getDimension(R.dimen.tab_thumbnail_width);
+        mThumbnailHeight = (int) getResources().getDimension(R.dimen.tab_thumbnail_height);
+
         mTitle = (TextView) findViewById(R.id.tab_title);
         mThumbnail = (ImageView) findViewById(R.id.thumbnail);
         mIncognitoIndicator = (ImageView) findViewById(R.id.incognito_indicator);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+//        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            }
+        }
+
+        int paddingHoriz = 2 * mPadding;
+        int paddingVert = 2 * mPadding;
+        int width = mThumbnailWidth + paddingHoriz;
+        int height = mThumbnailHeight + mTitleHeight + paddingVert;
+        setMeasuredDimension(width | MeasureSpec.EXACTLY, height | MeasureSpec.EXACTLY);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        final int count = getChildCount();
+        int x = 0, y = 0;
+        for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            if (child == null || child.getVisibility() == View.GONE) continue;
+            if (child == mThumbnail) {
+                x = mPadding;
+                y = mPadding;
+            } else if (child == mTitle) {
+                x = mPadding;
+                y = mPadding + mThumbnailHeight;
+            } else if (child == mIncognitoIndicator) {
+                x = mPadding + mThumbnailWidth - mTitleHeight;
+                y = mPadding + mThumbnailHeight;
+            }
+            child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());
+        }
+    }
+
+    int dpToPx(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 
     public void setTab(Tab tab) {
