e9104b3e0e9adcc8bc8634b506d3d2f602d9c062
Zbynek Muzik <zbynek.muzik@gmail.com>
2017-03-04 12:59:11 +0700

    pref keys refactoring

--- app/src/main/java/com/sabaibrowser/PreferenceKeys.java
+++ app/src/main/java/com/sabaibrowser/PreferenceKeys.java
@@ -18,23 +18,10 @@ package com.sabaibrowser;
 
 public interface PreferenceKeys {
 
-    static final String PREF_AUTOFILL_ACTIVE_PROFILE_ID = "autofill_active_profile_id";
-    static final String PREF_DEBUG_MENU = "debug_menu";
-
     // ----------------------
-    // Keys for accessibility_preferences.xml
+    // Keys for general_preferences.xml
     // ----------------------
-    static final String PREF_MIN_FONT_SIZE = "min_font_size";
-    static final String PREF_TEXT_SIZE = "text_size";
-    static final String PREF_TEXT_ZOOM = "text_zoom";
-    static final String PREF_DOUBLE_TAP_ZOOM = "double_tap_zoom";
-    static final String PREF_FORCE_USERSCALABLE = "force_userscalable";
-    static final String PREF_INVERTED = "inverted";
-    static final String PREF_INVERTED_CONTRAST = "inverted_contrast";
-
-    // ----------------------
-    // Keys for advanced_preferences.xml
-    // ----------------------
+    static final String PREF_HOMEPAGE = "homepage";
     static final String PREF_AUTOFIT_PAGES = "autofit_pages";
     static final String PREF_BLOCK_POPUP_WINDOWS = "block_popup_windows";
     static final String PREF_DEFAULT_TEXT_ENCODING = "default_text_encoding";
@@ -47,33 +34,6 @@ public interface PreferenceKeys {
     static final String PREF_SEARCH_ENGINE = "search_engine";
     static final String PREF_WEBSITE_SETTINGS = "website_settings";
     static final String PREF_ALLOW_APP_TABS = "allow_apptabs";
-
-    // ----------------------
-    // Keys for debug_preferences.xml
-    // ----------------------
-    static final String PREF_ENABLE_HARDWARE_ACCEL = "enable_hardware_accel";
-    static final String PREF_ENABLE_HARDWARE_ACCEL_SKIA = "enable_hardware_accel_skia";
-    static final String PREF_USER_AGENT = "user_agent";
-
-    // ----------------------
-    // Keys for general_preferences.xml
-    // ----------------------
-    static final String PREF_HOMEPAGE = "homepage";
-
-    // ----------------------
-    // Keys for hidden_debug_preferences.xml
-    // ----------------------
-    static final String PREF_ENABLE_LIGHT_TOUCH = "enable_light_touch";
-    static final String PREF_ENABLE_NAV_DUMP = "enable_nav_dump";
-    static final String PREF_ENABLE_TRACING = "enable_tracing";
-    static final String PREF_ENABLE_VISUAL_INDICATOR = "enable_visual_indicator";
-    static final String PREF_ENABLE_CPU_UPLOAD_PATH = "enable_cpu_upload_path";
-    static final String PREF_JAVASCRIPT_CONSOLE = "javascript_console";
-    static final String PREF_JS_ENGINE_FLAGS = "js_engine_flags";
-    static final String PREF_NORMAL_LAYOUT = "normal_layout";
-    static final String PREF_SMALL_SCREEN = "small_screen";
-    static final String PREF_WIDE_VIEWPORT = "wide_viewport";
-    static final String PREF_RESET_PRELOGIN = "reset_prelogin";
 
     // ----------------------
     // Keys for privacy_security_preferences.xml
@@ -89,6 +49,46 @@ public interface PreferenceKeys {
     static final String PREF_REMEMBER_PASSWORDS = "remember_passwords";
     static final String PREF_SAVE_FORMDATA = "save_formdata";
     static final String PREF_SHOW_SECURITY_WARNINGS = "show_security_warnings";
+
+    // ----------------------
+    // Keys for accessibility_preferences.xml
+    // ----------------------
+    static final String PREF_MIN_FONT_SIZE = "min_font_size";
+    static final String PREF_TEXT_SIZE = "text_size";
+    static final String PREF_TEXT_ZOOM = "text_zoom";
+    static final String PREF_DOUBLE_TAP_ZOOM = "double_tap_zoom";
+    static final String PREF_FORCE_USERSCALABLE = "force_userscalable";
+    static final String PREF_INVERTED = "inverted";
+    static final String PREF_INVERTED_CONTRAST = "inverted_contrast";
+
+    // ----------------------
+    // Keys for info_preferences.xml
+    // ----------------------
+    static final String PREF_VERSION = "version";
+    static final String PREF_BUILD = "build";
+    static final String PREF_LICENSES = "licenses";
+
+    // ------------------------- "invisible settings" start here -------------------------
+
+    static final String PREF_DEBUG_MENU = "debug_menu";
+
+    // ----------------------
+    // Keys for debug_preferences.xml
+    // ----------------------
+    static final String PREF_ENABLE_HARDWARE_ACCEL = "enable_hardware_accel";
+    static final String PREF_ENABLE_HARDWARE_ACCEL_SKIA = "enable_hardware_accel_skia";
+    static final String PREF_USER_AGENT = "user_agent";
+
+    // ----------------------
+    // Keys for hidden_debug_preferences.xml
+    // ----------------------
+    static final String PREF_ENABLE_LIGHT_TOUCH = "enable_light_touch";
+    static final String PREF_ENABLE_NAV_DUMP = "enable_nav_dump";
+    static final String PREF_JAVASCRIPT_CONSOLE = "javascript_console";
+    static final String PREF_JS_ENGINE_FLAGS = "js_engine_flags";
+    static final String PREF_NORMAL_LAYOUT = "normal_layout";
+    static final String PREF_WIDE_VIEWPORT = "wide_viewport";
+    static final String PREF_RESET_PRELOGIN = "reset_prelogin";
 
     // ----------------------
     // Keys for bandwidth_preferences.xml
